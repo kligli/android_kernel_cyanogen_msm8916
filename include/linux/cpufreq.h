fdec75dc8266f95a87c9591bc475f14de28efc65
beroid <beroid3@gmail.com>
2016-04-18 10:35:47 +0500

    cpufreq: add IMPULSE governor
    by @neobuddy89

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -442,6 +442,9 @@ extern struct cpufreq_governor cpufreq_gov_darkness;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
 extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_zzmoove)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
+extern struct cpufreq_governor cpufreq_gov_impulse;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
 #endif
 
 /*********************************************************************
