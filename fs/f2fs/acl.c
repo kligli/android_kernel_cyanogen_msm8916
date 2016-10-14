b11300448cd5d446798446e8c160061e9f429fcf
Jaegeuk Kim <jaegeuk@kernel.org>
2016-10-14 11:51:23 -0700

    f2fs: keep dirty inodes selectively for checkpoint
    This is to avoid no free segment bug during checkpoint caused by a number of
    dirty inodes.
    
    The case was reported by Chao like this.
    
    1. mount with lazytime option
    2. fragment space
    3. touch all files in the image
    4. umount
    
    In this case, we actually don't need to flush dirty inode to inode page during
    checkpoint.
    
    Reported-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/acl.c
+++ fs/f2fs/acl.c
@@ -299,7 +299,7 @@ int f2fs_init_acl(struct inode *inode, struct inode *dir, struct page *ipage,
 	if (error > 0)
 		error = f2fs_set_acl(inode, ACL_TYPE_ACCESS, acl, ipage);
 
-	f2fs_mark_inode_dirty_sync(inode);
+	f2fs_mark_inode_dirty_sync(inode, true);
 cleanup:
 	posix_acl_release(acl);
 	return error;
