6a2bbbebbc781da009b10db27e84be520e300893
vl197602 <radarga@gmail.com>
2016-10-30 23:43:03 +0300

    Add files via upload

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -454,9 +454,6 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BARRY_ALLEN)
 extern struct cpufreq_governor cpufreq_gov_barry_allen;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_barry_allen)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
-extern struct cpufreq_governor cpufreq_gov_yankactive;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankactive)
 #endif
 
 /*********************************************************************
