797b5201d0c45399ff6ac101bbedc8944a733b32
vl197602 <radarga@gmail.com>
2016-11-22 09:25:12 +0300

    Add PowerSuspend v1.5 Driver
    Android early_suspend/late_resume PM kernel driver framework has been
    deprecated by Google. This new powersuspend PM kernel driver is a
    replacement
    for it and existing early_suspend drivers can be easily adapted to use
    this
    new replacement driver
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: Shoaib0597 <Shoaib0595@gmail.com>
    
    powersuspend: fix logci derps :p
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: Shoaib0597 <Shoaib0595@gmail.com>
    
    kernel/power/powersuspend: remove userspace dependency from powersuspend
    
    make powersuspend not depend on a userspace initiator anymore, but use a
    hook
    in autosleep instead.
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: Shoaib0597 <Shoaib0595@gmail.com>
    
    kernel/power/powersuspend: add back userpace control w/ default kernel
    control
    
    make kernel / userspace mode switchable
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: Shoaib0597 <Shoaib0595@gmail.com>
    
    kernel/power/powersuspend: default to userspace for now
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: Shoaib0597 <Shoaib0595@gmail.com>
    
    kernel/power/powersuspend: LCD screen on/off hooks (Yank555.lu)
    
    - add an alternative hook for screen on / off detection in the display
    pannel driver
    - sleeps  ~0.1s sooner, wakes up ~1s later then autosleep
    - clean up source formatting a bit (Paul Reioux)
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: Shoaib0597 <Shoaib0595@gmail.com>
    
    kernel/power/powersuspend: cumulative update to version 1.5
    
    - fix hybrid-kernel mode cannot be set through sysfs
    
    kernel/power/powersuspend: new PM kernel driver for Android w/o
    early_suspend v1.4 (Yank555.lu)
    
    - add hybrid-kernel mode (autosleep and panel, first wins)
    - default to hybrid-kernel mode
    - harmonize debug message with my other stuff
    - include all 3 modes as default, so it's only commenting out to change
    
    kernel/power/powersuspend: new PM kernel driver for Android w/o
    early_suspend v1.3 (Yank555.lu)
    
    - fix stupid typo
    - add an alternative hook for screen on / off detection in the display
    panel driver
    (sleeps ~0.1s sooner, wakes up ~1s later then autosleep)
    
    kernel/power/powersuspend: new PM kernel driver for Android w/o
    early_suspend v1.2 (Yank555.lu)
    
    - make kernel / userspace mode switchable
    
    kernel/power/powersuspend: new PM kernel driver for Android w/o
    early_suspend v1.1 (Yank555.lu)
    
    - make powersuspend not depend on a userspace initiator anymore, but use
    a hook in autosleep instead.
    
    kernel/power/powersuspend: new PM kernel driver for Android w/o
    early_suspend (faux123)
    
    Android early_suspend/late_resume PM kernel driver framework has been
    deprecated by Google. This new powersuspend PM kernel driver is a
    replacement
    for it and existing early_suspend drivers can be easily adapted to use
    this
    new replacement driver
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: Shoaib0597 <Shoaib0595@gmail.com>

--- include/linux/powersuspend.h
+++ include/linux/powersuspend.h
@@ -33,6 +33,7 @@
 #define POWER_SUSPEND_INACTIVE	0
 #define POWER_SUSPEND_ACTIVE	1
 
+#define POWER_SUSPEND_AUTOSLEEP	0	// Use kernel autosleep as hook
 #define POWER_SUSPEND_USERSPACE	1	// Use fauxclock as trigger
 #define POWER_SUSPEND_PANEL	2	// Use display panel state as hook
 #define POWER_SUSPEND_HYBRID	3	// Use display panel state and autosleep as hook
@@ -46,10 +47,8 @@ struct power_suspend {
 void register_power_suspend(struct power_suspend *handler);
 void unregister_power_suspend(struct power_suspend *handler);
 
+void set_power_suspend_state_autosleep_hook(int new_state);
 void set_power_suspend_state_panel_hook(int new_state);
-#ifdef CONFIG_ADRENO_IDLER
-extern bool power_suspended;
-#endif
 
 #endif
 
